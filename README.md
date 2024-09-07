8a4a30e704f8e51514038ae49a4bca6c37b70631
Filipe Silva <filipematossilva@gmail.com>
2024-09-07 11:26:24 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Paul Graham Essays RSS
+
+RSS feed for https://paulgraham.com/articles.html.
+
+Add https://filipesilva.github.io/paulgraham-essays-rss/feed.rss to your RSS Reader to use it.
+
+The [official one](https://paulgraham.com/rss.html) stopped updating at "Superlinear Returns" so I made this one.
+
+
---
+++ src/user.clj
@@ -0,0 +1,36 @@
+(ns user
+  (:require [clojure.data.xml :as xml]
+            [clj-rss.core :as rss]
+            [net.cgrand.enlive-html :as enlive-html])
+  (:import (java.net URL)))
+
+(defn essays []
+  (map (fn [{:keys [attrs content]}]
+         (let [title (first content)
+               link  (:href attrs)]
+           {:title title
+            :link  (if (#{"Chapter 1 of Ansi Common Lisp"
+                          "Chapter 2 of Ansi Common Lisp"}
+                        title)
+                     ;; These are not relative links
+                     link
+                     (str "https://paulgraham.com/" link))}))
+       (-> "https://paulgraham.com/articles.html"
+           URL.
+           enlive-html/html-resource
+           (enlive-html/select [:td (enlive-html/nth-child 2) :a]))))
+
+(defn rss [essays]
+  (->> essays
+       (rss/channel {:title       "Paul Graham: Essays"
+                     :link        "http://www.paulgraham.com/"
+                     :description "Scraped feed provided by https://github.com/filipesilva/paulgraham-rss"})
+       xml/indent-str))
+
+(defn -main []
+  (->> (essays)
+       rss
+       (spit "./_site/feed.rss")))
+
+(comment
+  (-main))
