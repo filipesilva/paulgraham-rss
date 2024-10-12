2d4b0dbbd1b280a44121fa4a470f39fe3e37a6ff
Filipe Silva <filipematossilva@gmail.com>
2024-10-12 12:22:28 +0100

    strip cache buster in common lisp links
    They keep bubbling up as unread in my reader.

--- src/user.clj
+++ src/user.clj
@@ -1,20 +1,29 @@
 (ns user
-  (:require [clojure.data.xml :as xml]
-            [clj-rss.core :as rss]
-            [net.cgrand.enlive-html :as enlive-html])
+  (:require [clj-rss.core :as rss]
+            [clojure.data.xml :as xml]
+            [net.cgrand.enlive-html :as enlive-html]
+            [clojure.string :as str])
   (:import (java.net URL)))
+
+(defn relative?
+  [link]
+  (->> link
+       (re-find #"^https?://")
+       boolean
+       not))
+
+(defn strip-cache-buster
+  [link]
+  (str/replace link #"t=\d+" ""))
 
 (defn essays []
   (map (fn [{:keys [attrs content]}]
          (let [title (first content)
                link  (:href attrs)]
            {:title title
-            :link  (if (#{"Chapter 1 of Ansi Common Lisp"
-                          "Chapter 2 of Ansi Common Lisp"}
-                        title)
-                     ;; These are not relative links
-                     link
-                     (str "https://paulgraham.com/" link))}))
+            :link  (cond->> link
+                     true             strip-cache-buster
+                     (relative? link) (str "https://paulgraham.com/"))}))
        (-> "https://paulgraham.com/articles.html"
            URL.
            enlive-html/html-resource
@@ -34,4 +43,13 @@
        (spit "./_site/feed.rss")))
 
 (comment
-  (-main))
+
+  (relative? "https://sep.turbifycdn.com/ty/cdn/paulgraham/acl2.txt?t=1727692616&amp")
+  (relative? "avg.html")
+
+  (strip-cache-buster "https://sep.turbifycdn.com/ty/cdn/paulgraham/acl2.txt?t=1727692616&amp")
+
+  (-main)
+
+  ;;
+  )
